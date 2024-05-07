2baa6d753209fcf0a29d5e835edfd7aff9fd8d4a
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-05-07 16:55:32 +0300

    add route handlers and supabase for auth

--- ex1-helpdesk-app/app/(auth)/login/page.jsx
+++ ex1-helpdesk-app/app/(auth)/login/page.jsx
@@ -1,5 +1,20 @@
+"use client"
+
+import AuthForm from "../authForm"
+
 export default function Login() {
+
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault()
+        console.log({ email, password });
+    }
+
     return (
-        <main className="text-center">Login</main>
+        <main>
+            <h2 className="text-center">
+                Log in
+            </h2>
+            <AuthForm handleSubmit={handleSubmit} />
+        </main>
     )
 }
--- ex1-helpdesk-app/app/(auth)/signup/page.jsx
+++ ex1-helpdesk-app/app/(auth)/signup/page.jsx
@@ -1,5 +1,20 @@
+"use client"
+
+import AuthForm from "../authForm";
+
 export default function Signup() {
+
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault()
+        console.log('sign up', { email, password });
+    }
+
     return (
-        <main className="text-center">Signup</main>
+        <main className="text-center">
+            <h2 className="text-center">
+                Log in
+            </h2>
+            <AuthForm handleSubmit={handleSubmit} />
+        </main>
     )
 }
