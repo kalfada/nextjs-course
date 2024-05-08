baba062464bcdcac5b50d9c75d4964cd5d30e4d6
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-05-08 22:55:21 +0300

    add auth with supabase

--- ex1-helpdesk-app/app/(auth)/signup/page.jsx
+++ ex1-helpdesk-app/app/(auth)/signup/page.jsx
@@ -1,12 +1,30 @@
 "use client"
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
 import AuthForm from "../authForm";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
+    const router = useRouter()
+    const [errMsg, setErrMsg] = useState('')
 
     const handleSubmit = async (e, email, password) => {
         e.preventDefault()
-        console.log('sign up', { email, password });
+
+        const supabase = createClientComponentClient()
+        const { error } = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+                emailRedirectTo: `${location.origin}/api/auth/callback`
+            }
+        })
+        if (error) {
+            setErrMsg(error.message)
+        } else {
+            router.push('/verify')
+        }
     }
 
     return (
@@ -15,6 +33,9 @@ export default function Signup() {
                 Log in
             </h2>
             <AuthForm handleSubmit={handleSubmit} />
+            {errMsg && (
+                <div className="error">{errMsg}</div>
+            )}
         </main>
     )
 }
